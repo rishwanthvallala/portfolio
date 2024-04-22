f7e7695a980778187c0749c73f65a7765cbdfa95
Your Name <you@example.com>
2024-04-22 18:05:12 +0530

    basic replacements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,40 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { ThemeProvider } from '@/components/theme-provider'
-import { NavigationBar } from '@/components/nav-bar'
-import Link from 'next/link'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import { GeistSans } from "geist/font/sans";
+import type { Metadata } from 'next';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { NavigationBar } from '@/components/nav-bar';
+import Link from 'next/link';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GeistSans } from 'geist/font/sans';
 
 export const metadata: Metadata = {
     title: 'Daniel Wan',
     description: 'I plan, design, build, test and deploy software',
-}
+};
 
 export default function RootLayout({
     children,
 }: Readonly<{
-    children: React.ReactNode
+    children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
             <body className={GeistSans.className}>
                 <ThemeProvider
-                    attribute='class'
-                    defaultTheme='dark'
+                    attribute="class"
+                    defaultTheme="dark"
                     enableSystem
                     disableTransitionOnChange
                 >
                     <NavigationBar />
-                    <main className='container mx-auto'>
-                        {children}
-                    </main>
+                    <main className="container mx-auto">{children}</main>
                     <footer className="container mx-auto py-4">
-                        <Link href="https://github.com/KeluhingBavui/portfolio-v2" target="_blank">
+                        <Link
+                            href="https://github.com/rishwanthvallala/portfolio"
+                            target="_blank"
+                        >
                             <p className="text-center text-sm text-muted-foreground hover:text-white transition-all">
-                                © Designed & Built by Daniel Wan Jonathan · 2024
+                                © Designed & Built by Rishwanth Vallala · 2024
                             </p>
                         </Link>
                     </footer>
@@ -42,5 +43,5 @@ export default function RootLayout({
                 <SpeedInsights />
             </body>
         </html>
-    )
+    );
 }
